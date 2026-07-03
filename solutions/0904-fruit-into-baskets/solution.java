3fb7e9e40562285963f194e348a4e9df3d7c3b55
Krish Gupta <krishgupta28406@gmail.com>
2026-07-03 21:09:31 +0000

    Sync LeetCode submission - Fruit Into Baskets - Runtime - 7 ms (96.79%), Memory - 61.6 MB (99.68%)

---
+++ solutions/0904-fruit-into-baskets/solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int totalFruit(int[] fruits) {
+        int[] count = new int[fruits.length];
+        int left = 0, maxFruits = 0, distinct = 0;
+        
+        for (int right = 0; right < fruits.length; right++) {
+            if (count[fruits[right]] == 0) {
+                distinct++;
+            }
+            count[fruits[right]]++;
+            
+            while (distinct > 2) {
+                count[fruits[left]]--;
+                if (count[fruits[left]] == 0) {
+                    distinct--;
+                }
+                left++;
+            }
+            
+            int currentLen = right - left + 1;
+            if (currentLen > maxFruits) {
+                maxFruits = currentLen;
+            }
+        }
+        return maxFruits;
+    }
+}
