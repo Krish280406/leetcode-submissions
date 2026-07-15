0ef4fd660c324080c2c75781369c61f27bac88d5
Krish Gupta <krishgupta28406@gmail.com>
2026-07-15 02:21:14 +0000

    Sync LeetCode submission - Min Stack - Runtime - 31 ms (81.72%), Memory - 102.5 MB (20.39%)

---
+++ solutions/0155-min-stack/solution.java
@@ -0,0 +1,20 @@
+class MinStack {
+    Deque<Integer> stack    = new ArrayDeque<>();
+    Deque<Integer> minStack = new ArrayDeque<>();
+
+    public void push(int val) {
+        stack.push(val);
+        
+        int curMin = minStack.isEmpty() ? val
+                     : Math.min(val, minStack.peek());
+        minStack.push(curMin);
+    }
+
+    public void pop() {
+        stack.pop();
+        minStack.pop();  
+    }
+
+    public int top()    { return stack.peek(); }
+    public int getMin() { return minStack.peek(); }
+}
