07995c78fe9f78d51c4f74dbc58a887a92be59aa
Krish Gupta <krishgupta28406@gmail.com>
2026-08-03 16:54:43 +0000

    Sync LeetCode submission - Sort an Array - Runtime - 5 ms (98.58%), Memory - 73.1 MB (99.44%)

---
+++ solutions/0912-sort-an-array/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        int[] count = new int[100001];
+        
+        for (int num : nums) {
+            count[num + 50000]++;
+        }
+        
+        int index = 0;
+        for (int i = 0; i < 100001; i++) {
+            while (count[i] > 0) {
+                nums[index++] = i - 50000;
+                count[i]--;
+            }
+        }
+        
+        return nums;
+    }
+}
+
+
