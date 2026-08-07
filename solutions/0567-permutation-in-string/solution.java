ec52475b6e97c478afbf5e317c9326947af4356a
Krish Gupta <krishgupta28406@gmail.com>
2026-08-07 10:02:57 +0000

    Sync LeetCode submission - Permutation in String - Runtime - 5 ms (97.57%), Memory - 43.9 MB (74.16%)

---
+++ solutions/0567-permutation-in-string/solution.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int n = s1.length();
+        int m = s2.length();
+
+        if (n > m) {
+            return false;
+        }
+
+        int[] s1Freq = new int[26];
+        int[] windowFreq = new int[26];
+
+        for (int i = 0; i < n; i++) {
+            s1Freq[s1.charAt(i) - 'a']++;
+            windowFreq[s2.charAt(i) - 'a']++;
+        }
+
+        if (matches(s1Freq, windowFreq)) {
+            return true;
+        }
+
+        for (int i = n; i < m; i++) {
+            windowFreq[s2.charAt(i) - 'a']++;
+            windowFreq[s2.charAt(i - n) - 'a']--;
+
+            if (matches(s1Freq, windowFreq)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean matches(int[] a, int[] b) {
+        for (int i = 0; i < 26; i++) {
+            if (a[i] != b[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
